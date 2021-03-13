c4434aed99a36a23efa7ca260f430fe1c9036342
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-03-13 20:34:46 -0300

    feat: initializing cards

--- js/modules/renderCards.js
+++ js/modules/renderCards.js
@@ -1,14 +1,26 @@
 function renderPlants(target, plants) {
   console.log("plants", plants);
 
+  if (!plants.length)
+    document.querySelector(".no-results").classList.remove("hide");
+
   const cards = plants.map((plant) => {
+    const imgDiv = document.createElement("div");
+    imgDiv.setAttribute("class", "card__image");
+
     const img = document.createElement("img");
     img.setAttribute("src", plant.url);
-    img.setAttribute("class", "card_image");
+    imgDiv.append(img);
+
+    const nameDiv = document.createElement("div");
+    nameDiv.setAttribute("card", "card__name");
 
     const name = document.createElement("h3");
     name.innerHTML = plant.name;
-    name.setAttribute("card", "card__name");
+    nameDiv.append(name);
+
+    const priceIconDiv = document.createElement("div");
+    priceIconDiv.setAttribute("class", "class__price-icon");
 
     const price = document.createElement("h4");
     price.innerHTML = plant.price;
@@ -17,19 +29,24 @@ function renderPlants(target, plants) {
     const iconsDiv = document.createElement("div");
     iconsDiv.setAttribute("class", "card__icons");
 
+    priceIconDiv.append(price);
+    priceIconDiv.append(iconsDiv);
+
     const cardDiv = document.createElement("div");
     cardDiv.setAttribute("class", "card");
     if (plant.staff_favorite) cardDiv.setAttribute("class", "card__favorite");
-    cardDiv.append(img, name, price, iconsDiv);
+    cardDiv.append(imgDiv, nameDiv, priceIconDiv);
 
     return cardDiv;
   });
 
-  cards.map((card) => target.append(card));
+  document.querySelector(".no-results").classList.add("hide");
+
+  return cards.map((card) => target.append(card));
 }
 
 export default function renderResults() {
-  const resultsDiv = document.querySelector("#results");
+  const resultsDiv = document.querySelector("#cards");
 
   this.state.plants.length ? renderPlants(resultsDiv, this.state.plants) : null;
 }
